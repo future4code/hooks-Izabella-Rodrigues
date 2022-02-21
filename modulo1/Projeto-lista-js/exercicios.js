acfaa39690746022ee33b2c4dba0f583de0072cb
IZABELLA-MEZECK <bella13mezeck@gmail.com>
2022-02-20 23:55:00 -0300

    atualização lista JS

--- modulo1/Projeto-lista-js/exercicios.js
+++ modulo1/Projeto-lista-js/exercicios.js
@@ -31,7 +31,7 @@ function imprimeIdade() {
   // implemente sua lógica aqui
   const anoAtual = prompt ("Qual ano nós estamos?")
   const anoNascimento = prompt ("Qual ano você nasceu?")
-  console.log (Number(anoAtual) - Number(anoNascimento))
+  console.log(+anoAtual - +anoNascimento)
 }
 
 // EXERCÍCIO 03
@@ -53,9 +53,10 @@ console.log (`Meu nome é ${nomeUsuario}, tenho ${idadeUsuario} anos, e o meu em
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
   // implemente sua lógica aqui
-  const coresFavoritas = prompt ("Quais suas três cores favoritas?")
-  const coresArray = [coresFavoritas]
-  console.log(coresArray)
+  const cor1 = prompt ("Qual sua primeira cor favorita?")
+  const cor2 = prompt ("Qual sua segunda cor favorita?")
+  const cor3 = prompt ("Qual sua terceira cor favorita?")
+  console.log ([cor1, cor2, cor3])
 }
 
 // EXERCÍCIO 06
@@ -73,16 +74,13 @@ function calculaIngressosEspetaculo(custo, valorIngresso) {
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
-  const tamanho1 = (string1.lenght)
-  const tamanho2 = (string2.lenght)
-  console.log (tamanho1 === tamanho2)
+  return string1.length === string2.length
 }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
   // implemente sua lógica aqui
 return array[0]
-
 }
 
 // EXERCÍCIO 10
@@ -94,33 +92,76 @@ return array [array.length-1]
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
-  
+  const primeiro = array[0];
+  const ultimo = array[array.length - 1];
+  array[0] = ultimo;
+  array[array.length - 1] = primeiro;
+  return array
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
   // implemente sua lógica aqui
-string1.toLowerCase()
-string2.toLowerCase()
-return string1 === string2
+const string000 = string1.toLowerCase()
+const string001 = string2.toLowerCase()
+return string000 === string001
+}
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
   // implemente sua lógica aqui
+  //Passo 1: perguntar ao usuario as infos solicitadas.
+  const anoAtual = prompt ("Qual o ano atual?")
+  const anoNascimento = prompt ("Qual ano você nasceu?")
+  const anoEmissaoIdentidade = prompt ("Qual ano você emitiu sua identidade?")
+  //Passo 2: descobrir a idade do usuario
+  function idade (anoAtual, anoNascimento) {
+    const idade = +anoAtual - +anoNascimento
+    return idade
+  }
+  //Passo 3: comparar idade do usuario
+  const idade20 = idade <= 20
+  const idade20E50 = idade > 20 <= 50
+  const idade50 = idade > 50
+  //Passo 4: decobrir quando renovar
+  
 
 }
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
   // implemente sua lógica aqui
-
+  const anoInformado = ano
+  const multiplo400 = ano % 400
+  const multiplo4 = ano % 4
+  const multiplo100 = ano % 100
+  const boolean = multiplo400 === 0
+  const boolean2 = multiplo4 > 0
+  const boolean3 = multiplo100 === 0
+  const bisexto = !boolean2 
+  const bisexto2 = bisexto && boolean2
+  return bisexto2
 }
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
  // implemente sua lógica aqui
- const maiorIdade = prompt ("Você tem mais de 18 anos?")
- const ensinoMedio = prompt ("Você tem o ensino médio completo?")
- const horarioCurso = prompt ("Você tem disponibilidade exclusiva no horario do curso?")
- console.log (maiorIdade && ensinoMedio && horarioCurso)
+ function toBoolean(valor) {
+  return valor.toLowerCase() === 'sim' ? true : false;
+}
+
+// implemente sua lógica aqui
+const maiorIdade = prompt ("Você tem mais de 18 anos?")
+const ensinoMedio = prompt ("Você tem o ensino médio completo?")
+const horarioCurso = prompt ("Você tem disponibilidade exclusiva no horario do curso?")
+
+const isMaiorIdade = toBoolean(maiorIdade);
+const isEnsinoMedio = toBoolean(ensinoMedio);
+const isHorarioCurso = toBoolean(horarioCurso);
+
+const resultado = isMaiorIdade && isEnsinoMedio && isHorarioCurso;
+
+console.log(resultado);
+
+return resultado;
 }
